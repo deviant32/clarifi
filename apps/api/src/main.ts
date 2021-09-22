87f7b96467dbe424aff3654670ff19698baec65f
Joe Kolba <joe.kolba@quadrantbiosciences.com>
2021-09-22 15:50:25 -0400

    changed main

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,40 +1,23 @@
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { ServerlessNestjsApplicationFactory } from 'serverless-lambda-nestjs';
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import serverlessExpress from '@vendia/serverless-express';
+import { APIGatewayProxyHandler, Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app/app.module';
 
-async function bootstrap() {
+let server: Handler;
+
+async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  app.enableCors({
-    origin: '*',
-    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
-  });
-  const port = process.env.PORT || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
-  });
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
 
-// Run Nestjs application locally
-if (process.env.NX_CLI_SET) {
-  bootstrap();
-}
-
-// Run Nestjs application in AWS Lambda
-export const handler: APIGatewayProxyHandler = async (event, context) => {
-  const app = new ServerlessNestjsApplicationFactory<AppModule>(
-    AppModule,
-    {
-      // NestFactory.create's option object
-      cors: {
-        origin: '*',
-        allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
-      },
-    }
-  );
-  const result = await app.run(event, context);
-  return result;
+export const handler: Handler = async (
+  event: APIGatewayProxyHandler,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
 };
